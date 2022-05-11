e736b4bc3259a09129c6de80038fff5848061935
john.oehninger@gmail.com <john.oehninger@gmail.com>
2022-05-11 20:39:00 +0200

    Updates

---
+++ precache-manifest.551109258f9cf6b2b6e54d5f1979d801.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "89e1e1ccd6e17883eb66",
+    "url": "/near-marketplace/static/css/main.79296773.chunk.css"
+  },
+  {
+    "revision": "89e1e1ccd6e17883eb66",
+    "url": "/near-marketplace/static/js/main.77f324c0.chunk.js"
+  },
+  {
+    "revision": "ef95b9bdb90e39700fc4",
+    "url": "/near-marketplace/static/js/runtime~main.d1c0a66f.js"
+  },
+  {
+    "revision": "7df7534213096a146232",
+    "url": "/near-marketplace/static/css/2.9acc2a3c.chunk.css"
+  },
+  {
+    "revision": "7df7534213096a146232",
+    "url": "/near-marketplace/static/js/2.dd640d86.chunk.js"
+  },
+  {
+    "revision": "8114cd302583edb33ce7",
+    "url": "/near-marketplace/static/js/3.71641409.chunk.js"
+  },
+  {
+    "revision": "1d221eaf4000789f54c36047235dc71f",
+    "url": "/near-marketplace/static/media/my_logo.1d221eaf.png"
+  },
+  {
+    "revision": "1ed478a6b265d4b4f5c26bb063203588",
+    "url": "/near-marketplace/static/media/bootstrap-icons.1ed478a6.woff2"
+  },
+  {
+    "revision": "e4cc83ddcec34db65724eb56c5230b4c",
+    "url": "/near-marketplace/static/media/bootstrap-icons.e4cc83dd.woff"
+  },
+  {
+    "revision": "07e915bd6401684b472340a106d2b456",
+    "url": "/near-marketplace/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/near-marketplace/precache-manifest.551109258f9cf6b2b6e54d5f1979d801.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/near-marketplace/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
